b156a3e94e422c418c96ed543e58fabf6ce978e4
Pengcheng Xie <xpc@lsec.cc.ac.cn>
2024-02-13 16:21:42 +0800

    Update main.m

--- compute_one_step/main.m
+++ compute_one_step/main.m
@@ -6,26 +6,8 @@ global flag1
 flag1 = 0;
 global xinput
 
-% % % % % for ceshihaojici=1:1000
-
-% xinput=[2 0]';
-% 生成一个随机向量，范围在 -10 到 10 之间
-% % % % % xinputrand = (rand(2,1) * 20) - 10;
-% % % % % xinput = round(xinputrand);
-
 xinput=[0 7]';
 
 perfdata('plain');
 
-% % % % % Fs = readmatrix('F_data.csv')
-
-
-
-% % % % % if (Fs(1)<Fs(2)-1)&&(Fs(1)<Fs(3)-1)&&(Fs(1)<Fs(4)-1)&&(Fs(1)<Fs(5)-1)&&(Fs(4)<Fs(2)-1)&&(Fs(4)<Fs(3)-1)&&(Fs(4)<Fs(5)-1)
-% % % % % xinput
-% % % % % end
-
-% % % % % ceshihaojici=ceshihaojici+1;
-
-% % % % % end
 
